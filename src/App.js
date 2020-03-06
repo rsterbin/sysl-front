65445a373cbd4ee7741f8247fb2a94bf21366249
Reha Sterbin <reha@omniti.com>
2020-03-06 18:08:19 -0500

    Added bootstrap and set some test styles

--- src/App.js
+++ src/App.js
@@ -5,14 +5,13 @@ import Layout from './hoc/Layout/Layout';
 import PlayLogger from './containers/PlayLogger/PlayLogger';
 import Website from './containers/Website/Website';
 
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.module.css';
 
 function App() {
   return (
     <BrowserRouter>
         <Layout>
-            {/* <img src={logo} className="App-logo" alt="logo" /> */}
             <Switch>
                 <Route path="/play-logger" component={PlayLogger} />
                 <Route path="/" component={Website} />
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+// import Container from 'react-bootstrap/Container';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+
+import styles from './Layout.module.css';
 
 const layout = (props) => {
     return (
-        <div className="sysl">
-            <nav>
-                <NavLink to="/" exact>Website</NavLink>
-                <NavLink to="/play-logger">Play Logger</NavLink>
-            </nav>
-            {props.children}
+        <div className={styles.SYSL}>
+            <div className={styles.HeaderBar}>
+                <nav>
+                    <NavLink to="/" exact>Website</NavLink>
+                    <NavLink to="/play-logger">Play Logger</NavLink>
+                </nav>
+            </div>
+            <div className={styles.MainContent}>
+                {props.children}
+            </div>
         </div>
     );
 };
